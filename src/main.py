0d8e35ab3603cbcbcc3ec17c57adab9427e2298e
Anastasia Grigorovich <nstnlly@gmail.com>
2025-01-28 02:17:51 +0300

    Добавлена функция add_numbers

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.utils import add_numbers
+
 def main():
-    print("Hello, World!")
+    result = add_numbers(3, 5)
+    print(f"3 + 5 = {result}")
 
 if __name__ == "__main__":
     main()
